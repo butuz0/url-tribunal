f41f3d478f58bb18f3224e134ca1a9c4e7990067
Yaroslav Oryshchenko <y.o.oryshchenko@gmail.com>
2026-06-19 16:47:23 +0300

    add docstrings for enums

--- src/python/url_tribunal/core/enums.py
+++ src/python/url_tribunal/core/enums.py
@@ -1,7 +1,11 @@
+"""Global application enumerations."""
+
 from enum import StrEnum
 
 
 class Verdict(StrEnum):
+    """Security scan results for URLs and domains."""
+
     LEGIT = 'legit'
     SPAM = 'spam'
     MALWARE = 'malware'
